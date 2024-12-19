e8543466804167a894d9e8cb5637d70ce431f84f
Amolap-Dias <60244782+Amolap-Dias@users.noreply.github.com>
2024-12-19 17:46:50 -0300

    Add files via upload

---
+++ ITomadaDoisPinos.java
@@ -0,0 +1,4 @@
+public interface ITomadaDoisPinos {
+    
+    public void ligarTomadaDoisPinos();
+}
---
+++ ITomadaTresPinos.java
@@ -0,0 +1,4 @@
+public interface ITomadaTresPinos {
+    
+    public void ligarTomadaTresPinos();
+}
